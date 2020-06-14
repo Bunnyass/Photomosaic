6a3fff68cecee026d01ce1b78e919b5f813cd49d
Bunnyass <jayantnaik98@gmail.com>
2020-06-14 16:43:06 +0530

    Update

--- main.py
+++ main.py
@@ -1,280 +1,222 @@
-import time
-import itertools
-import random
 import sys
-
-import numpy as np
-import PIL.Image
+import os
+#pip install Pillow
 from PIL import Image
-from skimage import img_as_float
-from skimage.measure import compare_mse
-PIL.Image.MAX_IMAGE_PIXELS = 933120000
+from multiprocessing import Process, Queue, cpu_count
 
-def shuffle_first_items(lst, i):
-    if not i:
-        return lst
-    first_few = lst[:i]
-    remaining = lst[i:]
-    random.shuffle(first_few) 
-    return first_few + remaining
+TILE_SIZE      = 10		# height/width of mosaic tiles in pixels
+TILE_MATCH_RES = 5		# tile matching resolution (higher values give better fit but require more processing)
+ENLARGEMENT    = 8		# the mosaic image will be this many times wider and taller than the original
 
-def bound(low, high, value):
-    return max(low, min(high, value))
+TILE_BLOCK_SIZE = TILE_SIZE / max(min(TILE_MATCH_RES, TILE_SIZE), 1)
+WORKER_COUNT = max(cpu_count() - 1, 1)
+OUT_FILE = 'mosaic.jpeg'
+EOQ_VALUE = None
+
+class TileProcessor:
+	def __init__(self, tiles_directory):
+		self.tiles_directory = tiles_directory
+
+	def __process_tile(self, tile_path):
+		try:
+			img = Image.open(tile_path)
+			# tiles must be square, so get the largest square that fits inside the image
+			w = img.size[0]
+			h = img.size[1]
+			min_dimension = min(w, h)
+			w_crop = (w - min_dimension) / 2
+			h_crop = (h - min_dimension) / 2
+			img = img.crop((w_crop, h_crop, w - w_crop, h - h_crop))
+
+			large_tile_img = img.resize((TILE_SIZE, TILE_SIZE), Image.ANTIALIAS)
+			small_tile_img = img.resize((int(TILE_SIZE/TILE_BLOCK_SIZE), int(TILE_SIZE/TILE_BLOCK_SIZE)), Image.ANTIALIAS)
+
+			return (large_tile_img.convert('RGB'), small_tile_img.convert('RGB'))
+		except:
+			return (None, None)
+
+	def get_tiles(self):
+		large_tiles = []
+		small_tiles = []
+
+		print('Reading tiles from {}...'.format(self.tiles_directory))
+
+		# search the tiles directory recursively
+		for root, subFolders, files in os.walk(self.tiles_directory):
+			for tile_name in files:
+				print('Reading {:40.40}'.format(tile_name), flush=True, end='\r')
+				tile_path = os.path.join(root, tile_name)
+				large_tile, small_tile = self.__process_tile(tile_path)
+				if large_tile:
+					large_tiles.append(large_tile)
+					small_tiles.append(small_tile)
+
+
+		print('Processed {} tiles.'.format(len(large_tiles)))
+
+		return (large_tiles, small_tiles)
+
+class TargetImage:
+	def __init__(self, image_path):
+		self.image_path = image_path
+
+	def get_data(self):
+		print('Processing main image...')
+		img = Image.open(self.image_path)
+		w = img.size[0] * ENLARGEMENT
+		h = img.size[1]	* ENLARGEMENT
+		large_img = img.resize((w, h), Image.ANTIALIAS)
+		w_diff = (w % TILE_SIZE)/2
+		h_diff = (h % TILE_SIZE)/2
+		
+		# if necessary, crop the image slightly so we use a whole number of tiles horizontally and vertically
+		if w_diff or h_diff:
+			large_img = large_img.crop((w_diff, h_diff, w - w_diff, h - h_diff))
+
+		small_img = large_img.resize((int(w/TILE_BLOCK_SIZE), int(h/TILE_BLOCK_SIZE)), Image.ANTIALIAS)
+
+		image_data = (large_img.convert('RGB'), small_img.convert('RGB'))
+
+		print('Main image processed.')
+
+		return image_data
+
+class TileFitter:
+	def __init__(self, tiles_data):
+		self.tiles_data = tiles_data
+
+	def __get_tile_diff(self, t1, t2, bail_out_value):
+		diff = 0
+		for i in range(len(t1)):
+			#diff += (abs(t1[i][0] - t2[i][0]) + abs(t1[i][1] - t2[i][1]) + abs(t1[i][2] - t2[i][2]))
+			diff += ((t1[i][0] - t2[i][0])**2 + (t1[i][1] - t2[i][1])**2 + (t1[i][2] - t2[i][2])**2)
+			if diff > bail_out_value:
+				# we know already that this isn't going to be the best fit, so no point continuing with this tile
+				return diff
+		return diff
+
+	def get_best_fit_tile(self, img_data):
+		best_fit_tile_index = None
+		min_diff = sys.maxsize
+		tile_index = 0
+
+		# go through each tile in turn looking for the best match for the part of the image represented by 'img_data'
+		for tile_data in self.tiles_data:
+			diff = self.__get_tile_diff(img_data, tile_data, min_diff)
+			if diff < min_diff:
+				min_diff = diff
+				best_fit_tile_index = tile_index
+			tile_index += 1
+
+		return best_fit_tile_index
+
+def fit_tiles(work_queue, result_queue, tiles_data):
+	# this function gets run by the worker processes, one on each CPU core
+	tile_fitter = TileFitter(tiles_data)
+
+	while True:
+		try:
+			img_data, img_coords = work_queue.get(True)
+			if img_data == EOQ_VALUE:
+				break
+			tile_index = tile_fitter.get_best_fit_tile(img_data)
+			result_queue.put((img_coords, tile_index))
+		except KeyboardInterrupt:
+			pass
+
+	# let the result handler know that this worker has finished everything
+	result_queue.put((EOQ_VALUE, EOQ_VALUE))
 
 class ProgressCounter:
-    def __init__(self, total):
-        self.total = total
-        self.counter = 0
+	def __init__(self, total):
+		self.total = total
+		self.counter = 0
 
-    def update(self):
-        self.counter += 1
-        sys.stdout.write("Progress: %s%% %s" % (100 * self.counter / self.total, "\r"))
-        sys.stdout.flush()
-
-def img_mse(im1, im2):
-    """Calculates the root mean square error (RSME) between two images"""
-    try:
-        return compare_mse(img_as_float(im1), img_as_float(im2))
-    except ValueError:
-        print(f'RMS issue, Img1: {im1.size[0]} {im1.size[1]}, Img2: {im2.size[0]} {im2.size[1]}')
-        raise KeyboardInterrupt
-
-def resize_box_aspect_crop_to_extent(img, target_aspect, centerpoint=None):
-    width = img.size[0]
-    height = img.size[1]
-    if not centerpoint:
-        centerpoint = (int(width / 2), int(height / 2))
-
-    requested_target_x = centerpoint[0]
-    requested_target_y = centerpoint[1]
-    aspect = width / float(height)
-    if aspect > target_aspect:
-        # Then crop the left and right edges:
-        new_width = int(target_aspect * height)
-        new_width_half = int(new_width/2)
-        target_x = bound(new_width_half, width-new_width_half, requested_target_x)
-        left = target_x - new_width_half
-        right = target_x + new_width_half
-        resize = (left, 0, right, height)
-    else:
-        # ... crop the top and bottom: 
-        new_height = int(width / target_aspect)
-        new_height_half = int(new_height/2)
-        target_y = bound(new_height_half, height-new_height_half, requested_target_y)
-        top = target_y - new_height_half
-        bottom = target_y + new_height_half
-        resize = (0, top, width, bottom)
-    return resize
-
-def aspect_crop_to_extent(img, target_aspect, centerpoint=None):
-    '''
-    Crop an image to the desired perspective at the maximum size available.
-    Centerpoint can be provided to focus the crop to one side or another - 
-    eg just cut the left side off if interested in the right side.
-    target_aspect = width / float(height)
-    centerpoint = (width, height)
-    '''
-    resize = resize_box_aspect_crop_to_extent(img, target_aspect, centerpoint)
-    return img.crop(resize)
-
-class Config:
-    def __init__(self, tile_ratio=1920/800, tile_width=50, enlargement=8, color_mode='RGB'):
-        self.tile_ratio = tile_ratio # 2.4
-        self.tile_width = tile_width # height/width of mosaic tiles in pixels
-        self.enlargement = enlargement # mosaic image will be this many times wider and taller than original
-        self.color_mode = color_mode # mosaic image will be this many times wider and taller than original
-
-    @property
-    def tile_height(self):
-        return int(self.tile_width / self.tile_ratio)
-
-    @property
-    def tile_size(self):
-        return self.tile_width, self.tile_height # PIL expects (width, height)
-
-class TileBox:
-    """
-    Container to import, process, hold, and compare all of the tiles 
-    we have to make the mosaic with.
-    """
-    def __init__(self, tile_paths, config):
-        self.config = config
-        self.tiles = list()
-        self.prepare_tiles_from_paths(tile_paths)
-        
-    def __process_tile(self, tile_path):
-        with Image.open(tile_path) as i:
-            img = i.copy()
-        img = aspect_crop_to_extent(img, self.config.tile_ratio)
-        large_tile_img = img.resize(self.config.tile_size, Image.ANTIALIAS).convert(self.config.color_mode)
-        self.tiles.append(large_tile_img)
-        return True
-
-    def prepare_tiles_from_paths(self, tile_paths):
-        print('Reading tiles from provided list...')
-        progress = ProgressCounter(len(tile_paths))
-        for tile_path in tile_paths:
-            progress.update()
-            self.__process_tile(tile_path)          
-        print('Processed tiles.')
-        return True
-
-    def best_tile_block_match(self, tile_block_original):
-        match_results = [img_mse(t, tile_block_original) for t in self.tiles] 
-        best_fit_tile_index = np.argmin(match_results)
-        return best_fit_tile_index
-
-    def best_tile_from_block(self, tile_block_original, reuse=False):
-        if not self.tiles:
-            print('Ran out of images.')
-            raise KeyboardInterrupt
-        
-        #start_time = time.time()
-        i = self.best_tile_block_match(tile_block_original)
-        #print("BLOCK MATCH took --- %s seconds ---" % (time.time() - start_time))
-        match = self.tiles[i].copy()
-        if not reuse:
-            del self.tiles[i]
-        return match
-
-class SourceImage:
-    """Processing original image - scaling and cropping as needed."""
-    def __init__(self, image_path, config):
-        print('Processing main image...')
-        self.image_path = image_path
-        self.config = config
-
-        with Image.open(self.image_path) as i:
-            img = i.copy()
-        w = img.size[0] * self.config.enlargement
-        h = img.size[1]	* self.config.enlargement
-        large_img = img.resize((w, h), Image.ANTIALIAS)
-        w_diff = (w % self.config.tile_width)/2
-        h_diff = (h % self.config.tile_height)/2
-        
-        # if necesary, crop the image slightly so we use a 
-        # whole number of tiles horizontally and vertically
-        if w_diff or h_diff:
-            large_img = large_img.crop((w_diff, h_diff, w - w_diff, h - h_diff))
-
-        self.image =  large_img.convert(self.config.color_mode)
-        print('Main image processed.')
+	def update(self):
+		self.counter += 1
+		print("Progress: {:04.1f}%".format(100 * self.counter / self.total), flush=True, end='\r')
 
 class MosaicImage:
-    """Holder for the mosaic"""
-    def __init__(self, original_img, target, config):
-        self.config = config
-        self.target = target
-        # Lets just start with original image, scaled up, instead of a blank one
-        self.image = original_img
-        # self.image = Image.new(original_img.mode, original_img.size)
-        self.x_tile_count = int(original_img.size[0] / self.config.tile_width)
-        self.y_tile_count = int(original_img.size[1] / self.config.tile_height)
-        self.total_tiles  = self.x_tile_count * self.y_tile_count
-        print(f'Mosaic will be {self.x_tile_count:,} tiles wide and {self.y_tile_count:,} tiles high ({self.total_tiles:,} total).')
+	def __init__(self, original_img):
+		self.image = Image.new(original_img.mode, original_img.size)
+		self.x_tile_count = int(original_img.size[0] / TILE_SIZE)
+		self.y_tile_count = int(original_img.size[1] / TILE_SIZE)
+		self.total_tiles  = self.x_tile_count * self.y_tile_count
 
-    def add_tile(self, tile, coords):
-        """Adds the provided image onto the mosiac at the provided coords."""
-        try:
-            self.image.paste(tile, coords)
-        except TypeError as e:
-            print('Maybe the tiles are not the right size. ' + str(e))
+	def add_tile(self, tile_data, coords):
+		img = Image.new('RGB', (TILE_SIZE, TILE_SIZE))
+		img.putdata(tile_data)
+		self.image.paste(img, coords)
 
-    def save(self):
-        self.image.save(self.target)
+	def save(self, path):
+		self.image.save(path)
 
-def coords_from_middle(x_count, y_count, y_bias=1, shuffle_first=0, ):
-    '''
-    Lets start in the middle where we have more images.
-    And we dont get "lines" where the same-best images
-    get used at the start.
-    y_bias - if we are using non-square coords, we can
-        influence the order to be closer to the real middle.
-        If width is 2x height, y_bias should be 2.
-    shuffle_first - We can suffle the first X coords
-        so that we dont use all the same-best images
-        in the same spot -  in the middle
-    from movies.mosaic_mem import coords_from_middle
-    x = 10
-    y = 10
-    coords_from_middle(x, y, y_bias=2, shuffle_first=0)
-    '''
-    x_mid = int(x_count/2)
-    y_mid = int(y_count/2)
-    coords = list(itertools.product(range(x_count), range(y_count)))
-    coords.sort(key=lambda c: abs(c[0]-x_mid)*y_bias + abs(c[1]-y_mid))
-    coords = shuffle_first_items(coords, shuffle_first)
-    return coords
-    
+def build_mosaic(result_queue, all_tile_data_large, original_img_large):
+	mosaic = MosaicImage(original_img_large)
 
-def create_mosaic(source_path, target, tile_ratio=1920/800, tile_width=75, enlargement=8, reuse=True, color_mode='RGB', tile_paths=None, shuffle_first=30):
-    """Forms an mosiac from an original image using the best
-    tiles provided. This reads, processes, and keeps in memory
-    a copy of the source image, and all the tiles while processing.
-    Arguments:
-    source_path -- filepath to the source image for the mosiac
-    target -- filepath to save the mosiac
-    tile_ratio -- height/width of mosaic tiles in pixels
-    tile_width -- width of mosaic tiles in pixels
-    enlargement -- mosaic image will be this many times wider and taller than the original
-    reuse -- Should we reuse tiles in the mosaic, or just use each tile once?
-    color_mode -- L for greyscale or RGB for color
-    tile_paths -- List of filepaths to your tiles
-    shuffle_first -- Mosiac will be filled out starting in the center for best effect. Also, 
-        we will shuffle the order of assessment so that all of our best images aren't 
-        necessarily in one spot.
-    """
-    config = Config(
-        tile_ratio = tile_ratio,		# height/width of mosaic tiles in pixels
-        tile_width = tile_width,		# height/width of mosaic tiles in pixels
-        enlargement = enlargement,	    # the mosaic image will be this many times wider and taller than the original
-        color_mode = color_mode,	    # L for greyscale or RGB for color
-    )
-    # Pull in and Process Original Image
-    print('Setting Up Target image')
-    source_image = SourceImage(source_path, config)
+	active_workers = WORKER_COUNT
+	while True:
+		try:
+			img_coords, best_fit_tile_index = result_queue.get()
 
-    # Setup Mosaic
-    mosaic = MosaicImage(source_image.image, target, config)
+			if img_coords == EOQ_VALUE:
+				active_workers -= 1
+				if not active_workers:
+					break
+			else:
+				tile_data = all_tile_data_large[best_fit_tile_index]
+				mosaic.add_tile(tile_data, img_coords)
 
-    # Assest Tiles, and save if needed, returns directories where the small and large pictures are stored
-    print('Assessing Tiles')
-    tile_box = TileBox(tile_paths, config)
+		except KeyboardInterrupt:
+			pass
 
-    try:
-        progress = ProgressCounter(mosaic.total_tiles)
-        for x, y in coords_from_middle(mosaic.x_tile_count, mosaic.y_tile_count, y_bias=config.tile_ratio, shuffle_first=shuffle_first):
-            progress.update()
+	mosaic.save(OUT_FILE)
+	print('\nFinished, output is in', OUT_FILE)
 
-            # Make a box for this sector
-            box_crop = (x * config.tile_width, y * config.tile_height, (x + 1) * config.tile_width, (y + 1) * config.tile_height)
+def compose(original_img, tiles):
+	print('Building mosaic, press Ctrl-C to abort...')
+	original_img_large, original_img_small = original_img
+	tiles_large, tiles_small = tiles
 
-            # Get Original Image Data for this Sector
-            comparison_block = source_image.image.crop(box_crop)
+	mosaic = MosaicImage(original_img_large)
 
-            # Get Best Image name that matches the Orig Sector image
-            tile_match = tile_box.best_tile_from_block(comparison_block, reuse=reuse)
-            
-            # Add Best Match to Mosaic
-            mosaic.add_tile(tile_match, box_crop)
+	all_tile_data_large = [list(tile.getdata()) for tile in tiles_large]
+	all_tile_data_small = [list(tile.getdata()) for tile in tiles_small]
 
-            # Saving Every Sector
-            mosaic.save() 
+	work_queue   = Queue(WORKER_COUNT)	
+	result_queue = Queue()
 
-    except KeyboardInterrupt:
-        print('\nStopping, saving partial image...')
+	try:
+		# start the worker processes that will build the mosaic image
+		Process(target=build_mosaic, args=(result_queue, all_tile_data_large, original_img_large)).start()
 
-    finally:
-        mosaic.save()
+		# start the worker processes that will perform the tile fitting
+		for n in range(WORKER_COUNT):
+			Process(target=fit_tiles, args=(work_queue, result_queue, all_tile_data_small)).start()
 
-create_mosaic(
-    source_path=r"C:\Users\Bunnyass\Documents\Python\Projects\Photomosaic\Media\source.jpg", 
-    target=r"C:\Users\Bunnyass\Documents\Python\Projects\Photomosaic\Media\output.jpg", 
-    tile_ratio=1920/800, # Crop tiles to be height/width ratio
-    tile_width=300, # Tile will be scaled
-    enlargement=20, # Mosiac will be this times larger than original
-    reuse=False, # Should tiles be used multiple times?
-    color_mode='L',  # RGB (color) L (greyscale)
-    tile_paths=[r"C:\Users\Bunnyass\Documents\Python\Projects\Photomosaic\Media\Tiles\django001.jpg" , ...],
-    shuffle_first=30,
-)
+		progress = ProgressCounter(mosaic.x_tile_count * mosaic.y_tile_count)
+		for x in range(mosaic.x_tile_count):
+			for y in range(mosaic.y_tile_count):
+				large_box = (x * TILE_SIZE, y * TILE_SIZE, (x + 1) * TILE_SIZE, (y + 1) * TILE_SIZE)
+				small_box = (x * TILE_SIZE/TILE_BLOCK_SIZE, y * TILE_SIZE/TILE_BLOCK_SIZE, (x + 1) * TILE_SIZE/TILE_BLOCK_SIZE, (y + 1) * TILE_SIZE/TILE_BLOCK_SIZE)
+				work_queue.put((list(original_img_small.crop(small_box).getdata()), large_box))
+				progress.update()
+
+	except KeyboardInterrupt:
+		print('\nHalting, saving partial image please wait...')
+
+	finally:
+		# put these special values onto the queue to let the workers know they can terminate
+		for n in range(WORKER_COUNT):
+			work_queue.put((EOQ_VALUE, EOQ_VALUE))
+
+def mosaic(img_path, tiles_path):
+	image_data = TargetImage(img_path).get_data()
+	tiles_data = TileProcessor(tiles_path).get_tiles()
+	compose(image_data, tiles_data)
+
+if __name__ == '__main__':
+	if len(sys.argv) < 3:
+		print('Usage: {} <image> <tiles directory>\r'.format(sys.argv[0]))
+	else:
+		mosaic(sys.argv[1], sys.argv[2])
