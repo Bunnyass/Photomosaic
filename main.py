edfd8cf215db10e2aba8854c16fe0a204fd414f9
Jayant Kumar Naik <jayant.naik@magicbricks.com>
2024-04-21 15:37:55 +0530

    Reduced Enlargement

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ To generate a sequence of tiles(screenshots) from a movie
 
 TILE_SIZE      = 50		# height/width of mosaic tiles in pixels (Smaller the value finer will be the result)
 TILE_MATCH_RES = 5		# tile matching resolution (higher values give better fit but require more processing)
-ENLARGEMENT    = 8		# the mosaic image will be this many times wider and taller than the original
+ENLARGEMENT    = 6		# the mosaic image will be this many times wider and taller than the original
 
 TILE_BLOCK_SIZE = TILE_SIZE / max(min(TILE_MATCH_RES, TILE_SIZE), 1)
 WORKER_COUNT = max(cpu_count() - 1, 1)
